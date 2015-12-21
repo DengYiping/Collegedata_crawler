01cb0a531fb323cf35a45856cdb36d105377e13f
DengYiping <scottdeng@live.cn>
2015-12-21 17:32:31 -0600

    Initial Commit

---
+++ Collegedata_crawler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Collegedata_crawler
+//
+//  Created by Scott Deng on 12/21/15.
+//  Copyright © 2015 Scott Deng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
